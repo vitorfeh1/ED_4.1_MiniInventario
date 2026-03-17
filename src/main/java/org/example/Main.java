36fd99edd9a6ef35b5fb7fecd04b928192b3dc19
Vitor Miranda <vitorfeh1@gmail.com>
2026-03-17 13:49:51 +0100

    feat: añade estructura del proyecto y mejora salida inicial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("MiniInventario iniciado correctamente.");
+        // Nueva linea
+        System.out.println("Versión inicial con gestión básica de productos.");
     }
 }

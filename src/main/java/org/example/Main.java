20901143004b6685d808b4644b241a54b0b54a87
Vitor Miranda <vitorfeh1@gmail.com>
2026-04-21 14:16:01 +0200

    feat: incorpora mejora en Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("MiniInventario iniciado correctamente.");
-        // Nueva linea
-        System.out.println("Versión inicial con gestión básica de productos.");
+    public static void buscarProducto(ArrayList<String> productos, String nombreBuscado) {
+        boolean encontrado = false;
+        for (String producto : productos) {
+            if (producto.equalsIgnoreCase(nombreBuscado)) {
+                encontrado = true;
+                break;
+            }
+        }
+        if (encontrado) {
+            System.out.println("Producto encontrado en el inventario.");
+        } else {
+            System.out.println("El producto no existe en el inventario.");
+        }
     }
 }
